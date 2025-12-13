ec2376f1a454533487d9fa727f9f17c697ea7097
shivaganga <shivagangashivaganga853@gmail.com>
2025-12-13 06:03:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,29 +1,28 @@
-  // Import Firebase SDK
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
-// Your Firebase configuration
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0
 const firebaseConfig = {
   apiKey: "AIzaSyCVsTNfI-nlaKqx7BxEVSoU9E7qtdJc5Mw",
   authDomain: "women-safety-b01ae.firebaseapp.com",
   projectId: "women-safety-b01ae",
-  storageBucket: "women-safety-b01ae.appspot.com",
+  storageBucket: "women-safety-b01ae.firebasestorage.app",
   messagingSenderId: "185083347571",
-  appId: "1:185083347571:web:6776fc6f294b3912a5e006"
+  appId: "1:185083347571:web:6776fc6f294b3912a5e006",
+  
 };
-
-// Initialize Firebase App
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Authentication
 const auth = getAuth(app);
 
-// Initialize Firestore
-const db = getFirestore(app);
+// Export auth so it can be used in other files
+export { auth };
 
-// Export auth & db for use in other files
-export { auth, db };
 
 
 
